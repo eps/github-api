155833c9dfc77cf3dd59882eb3f3f71d03368759
Herman Chan <hermchan89@gmail.com>
2016-07-12 02:44:30 -0700

    added api endpoint, successfully retrieved json data

--- app.js
+++ app.js
@@ -2,13 +2,36 @@ console.log("Sanity Check: JS is working");
 
 var github_endpoint = "https://api.github.com/users/";
 var username;
-var fullname;
 
 $(document).ready(function (){
   $('#ghsubmit').on('click', function (e) {
     e.preventDefault();
     var username = $('#ghusername').val();
     console.log('search button clicked for ' + username);
+    callGithubAPI();
   });
+
+  function callGithubAPI(){
+    $.ajax({
+      method: "GET",
+      url: github_endpoint + $('#ghusername').val(),
+      success: displayGithubInfo,
+      error: handleError
+    });
+  }
+
+  function displayGithubInfo(json) {
+    // else we have a user and we display their info
+    console.log(json);
+  }
+
+  function handleError(xhr, status, errorThrown) {
+    console.log("Error " + errorThrown);
+    console.log("Status: " + status);
+    console.log(xhr);
+    if(errorThrown == "Not Found" ) {
+      $('#ghinfo').append("<h2>No User Info Found</h2>");
+    }
+  }
 
 });

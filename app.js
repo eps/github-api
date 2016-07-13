7bb6aff78e3ceb7f69e37872d1df4664178340cb
Herman Chan <hermchan89@gmail.com>
2016-07-12 17:29:20 -0700

    displayed user data onto index.html

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log("Sanity Check: JS is working");
 
 var github_endpoint = "https://api.github.com/users/";
 var username;
+var fullname;
 
 $(document).ready(function (){
   $('#ghsubmit').on('click', function (e) {
@@ -23,6 +24,29 @@ $(document).ready(function (){
   function displayGithubInfo(json) {
     // else we have a user and we display their info
     console.log(json);
+    var fullname   = json.name;
+    var username   = json.login;
+    var aviurl     = json.avatar_url;
+    var profileurl = json.html_url;
+    var location   = json.location;
+    var followersnum = json.followers;
+    var followingnum = json.following;
+    var reposnum     = json.public_repos;
+
+    if (fullname !== null) {
+      console.log('name is not null, current name is:', fullname);
+    } else {
+     fullname = json.login;
+     console.log('new fullname', json.login);
+    }
+
+    var outhtml = '<h2>'+fullname+' <span class="smallname">(@<a href="'+profileurl+'" target="_blank">'+username+'</a>)</span></h2>';
+   outhtml = outhtml + '<div class="ghcontent"><div class="avi"><a href="'+profileurl+'" target="_blank"><img src="'+aviurl+'" width="80" height="80" alt="'+username+'"></a></div>';
+   outhtml = outhtml + '<p>Followers: '+followersnum+' - Following: '+followingnum+'<br>Repos: '+reposnum+'</p></div>';
+   outhtml = outhtml + '<div class="repolist clearfix">';
+
+   $('#ghinfo').append(outhtml);
+
   }
 
   function handleError(xhr, status, errorThrown) {
